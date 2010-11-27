99843de0e50c29907ddfa3da1af683dfc14d34ff
Robert Jordens <jordens@phys.ethz.ch>
2010-11-27 11:48:39 +0100

    flickr: twisted authentication methods

--- eyefi/twisted_flickrapi.py
+++ eyefi/twisted_flickrapi.py
@@ -18,8 +18,6 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-# FIXME: bad, bad, bad: we are overriding private methods here
-# the actual changes are some 10 lines only, the rest is duplicated code
 # The original code has the following license/copyright:
 # 
 # This code is subject to the Python licence, as can be read on
@@ -51,7 +49,12 @@
 from twisted.web.client import getPage
 from twisted.internet.defer import succeed
 
-from flickrapi import FlickrAPI, rest_parsers, LOG
+from flickrapi import FlickrAPI, rest_parsers, LOG, FlickrError
+
+
+# FIXME: bad, bad, bad: we are overriding private methods here
+# FIXME: the actual changes are some 20 lines only, the rest is
+# duplicated code
 
 class TwistedFlickrAPI(FlickrAPI):
     def _FlickrAPI__flickr_call(self, **kwargs):
@@ -85,27 +88,109 @@ class TwistedFlickrAPI(FlickrAPI):
     def _FlickrAPI__send_multipart(self, url, body, progress_callback=None):
         assert not progress_callback, \
             "twisted upload/replace does not support progress callbacks yet"
+        # would be like
+        # http://twistedmatrix.com/pipermail/twisted-web/2007-January/003253.html
         LOG.debug("Uploading to %s" % url)
         reply = getPage(url, method="POST", postdata=str(body),
                 headers=dict([body.header()]))
         return reply
 
+    def trait_names(self):
+        """
+        ipython introspection needs to be synchornous, disable it
+        """
+        return None
+
+    def get_token_part_one(self, perms="read"):
+        token = succeed(self.token_cache.token)
+        @token.addCallback
+        def check(token):
+            if not token:
+                return None # need new one
+            LOG.debug("Trying cached token '%s'" % token)
+            rsp = self.auth_checkToken(auth_token=token, format='xmlnode')
+            @rsp.addCallback
+            def check_get(rsp):
+                tokenPerms = rsp.auth[0].perms[0].text
+                if tokenPerms == "read" and perms != "read":
+                    return None # need new
+                elif tokenPerms == "write" and perms == "delete":
+                    return None # need new
+                return token # is good
+            @rsp.addErrback
+            def check_err(err):
+                err.trap(FlickrError)
+                LOG.debug("Cached token invalid")
+                self.token_cache.forget()
+                return None # need new
+            return rsp # automatic deferred chaining
+        @token.addCallback
+        def need_new(token):
+            if token:
+                return token, None # good token, no new frob
+            LOG.debug("Getting frob for new token")
+            rsp = self.auth_getFrob(auth_token=None, format='xmlnode')
+            @rsp.addCallback
+            def valid_frob(rsp):
+                frob = rsp.frob[0].text
+                self.validate_frob(frob, perms)
+                return token, frob
+            return rsp # automatic deferred chaining
+        return token
+
+    def get_token(self, frob):
+        rsp = self.auth_getToken(frob=frob, auth_token=None, format='xmlnode')
+        @rsp.addCallback
+        def extract_token(rsp):
+            token = rsp.auth[0].token[0].text
+            LOG.debug("get_token: new token '%s'" % token)
+            # store the auth info for next time
+            self.token_cache.token = token
+            return token
+        return rsp
+
+    def authenticate_console(self, perms='read'):
+        d = self.get_token_part_one(perms)
+        @d.addCallback
+        def wait(arg):
+            token, frob = arg
+            if not token:
+                # raw_inputs message gets swallowed by log
+                print "Press ENTER after you authorized this program" 
+                raw_input()
+            return token, frob
+        d.addCallback(self.get_token_part_two)
+        return d
+
 
 def main():
     from twisted.internet import reactor
     from twisted.python import log
-    import sys
-    log.startLogging(sys.stdout)
+    import logging
+    log.PythonLoggingObserver().start()
+    logging.getLogger().setLevel(level=logging.DEBUG)
+    LOG.setLevel(level=logging.DEBUG)
 
     api_key = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
     api_secret = "XXXXXXXXXXXX"
-    
+
     flickr = TwistedFlickrAPI(api_key, api_secret)
-    # flickr.upload("pic", is_public="0"
-    #       ).addBoth(log.msg)
+
+    #flickr.authenticate_console("write"
+    #    ).addCallback(log.msg, "<- got token"
+    #    ).addBoth(lambda _: reactor.callLater(0, reactor.stop)
+    #    )
+
+    #flickr.upload("test.jpg", is_public="0"
+    #    ).addBoth(log.msg
+    #    ).addBoth(lambda _: reactor.callLater(0, reactor.stop)
+    #    )
+
     flickr.photos_search(user_id='73509078@N00', per_page='10'
-            ).addBoth(log.msg
-            ).addBoth(lambda e: reactor.callLater(0, reactor.stop))
+        ).addBoth(log.msg
+        ).addBoth(lambda _: reactor.callLater(0, reactor.stop)
+        )
+    
     reactor.run()
 
 
